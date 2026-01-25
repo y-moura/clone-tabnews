1db12c33bcb07e8eacccd8757b1b5eeebd68b237
Yuri Moura <contatoyurimoura@gmail.com>
2026-01-25 04:00:26 -0300

    finish `/status` endpoint; closes #26, closed #13

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM");
-  console.log(result.rows);
-  response.status(200).json({ chave: "foiii média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseOpenConnections = await database.query({
+    text: "SELECT COUNT(*)::INT FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+  const maxConnValue = databaseMaxConnections.rows[0].max_connections;
+  const openConnValue = databaseOpenConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnValue),
+        open_connections: openConnValue,
+      },
+    },
+  });
 }
 
 export default status;
